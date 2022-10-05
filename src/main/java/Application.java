aa7e675ad42b209d552c590042ae304a1d0b6b02
up202108777 <up202108777@up.pt>
2022-10-05 16:23:37 +0100

    teste da nova class Game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,28 +8,10 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Game first = new Game() ;
+        first.run();
 
-
-        try {
-            TerminalSize TAMANHO_TERMINAL = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(TAMANHO_TERMINAL);
-
-            Terminal terminal = terminalFactory.createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
-
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
     }
 }
