e94c2a0cab070f79d62b8e5d95d3c675716b8cbe
up202108777 <up202108777@up.pt>
2022-10-05 15:54:50 +0100

    Criado um terminal e uma screen com o Lanterna, screen tem um x

---
+++ src/main/java/Application.java
@@ -0,0 +1,35 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) {
+
+
+        try {
+            TerminalSize TAMANHO_TERMINAL = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(TAMANHO_TERMINAL);
+
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
