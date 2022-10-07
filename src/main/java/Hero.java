0be010d39e6c5d275684063d4b0ccb9826aa8873
up202108777 <up202108777@up.pt>
2022-10-07 10:58:55 +0100

    created Hero Class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,32 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+
 public class Hero {
+    private Position position = new Position(10,10);
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position pos){
+        this.position = pos ;
+    }
+
+    public Position moveUp() {return new Position(position.getX(), position.getY() - 1);}                                               // hero movement
+    public Position moveDown() {
+        return new Position(position.getX(), position.getY() + 1);
+    }
+    public Position moveRight() {
+        return new Position(position.getX() + 1, position.getY());
+    }
+    public Position moveLeft() {
+        return new Position(position.getX() - 1, position.getY());
+    }
+
+    public void draw(Screen screen_){
+        screen_.setCharacter( position.getX() , position.getY(), TextCharacter.fromCharacter('X')[0]);
+
+    }
+
+
 }
