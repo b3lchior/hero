2a1a79fa41506cddaaab295ca8f24bd792553fe7
up202108777 <up202108777@up.pt>
2022-10-07 10:58:17 +0100

    created Position Class

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,2 +1,15 @@
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x_ , int y_) {
+        x = x_;
+        y = y_;
+    }
+
+    public int getX() { return x;}                                  //getters and setters
+    public void setX(int x) { this.x = x;}
+    public int getY() { return y;}
+    public void setY(int y) { this.y = y;}
+
 }
