a2f4a019956ab8d6f5b2ec51bfb5d7f7d517f9de
up202108777 <up202108777@up.pt>
2022-10-08 13:25:46 +0100

    created class Coin

--- src/main/java/Coin.java
+++ src/main/java/Coin.java
@@ -1,2 +1,16 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
 public class Coin extends Element{
+
+    public Coin(int x , int y){
+        super (x, y);
+    }
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FAFAD2"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(this.getPosition().getX(), this.getPosition().getY()), "0");
+    }
 }
